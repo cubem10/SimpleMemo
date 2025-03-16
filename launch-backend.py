63477777d1c1f355630aaf584026564b6ba1b833
cubem10 <cubem10@kakao.com>
2025-03-17 01:35:15 +0900

    Implement basic structures

---
+++ launch-backend.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, request, jsonify
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+bcrypt = Bcrypt()
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
+app.config["JWT_SECRET_KEY"] = "GoN_MeMo"
+app.config["JWT_VERIFY_SUB"] = False
+
+class User(db.Model): 
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
+
+class Memo(db.Model): 
+    __tablename__ = 'memo'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.String(140), nullable=True)
+
+@app.route('/')
+def index(): 
+    return render_template('index.html')
+
+@app.route('/register', methods=['POST'])
+def register(): 
+    data = request.json
+    if not data['username'] or not data['password']: 
+        return jsonify({"message": "Please fill in all the fields"})
+    hashed_pw = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    new_user = User(username=data['username'], password_hash=hashed_pw)
+    if User.query.filter_by(username=data['username']).first() is not None: 
+        return jsonify({"message": f"User with username {data['username']} already exists"}), 403
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User registered successfully"})
+
+@app.route('/login', methods=['POST'])
+def login(): 
+    data = request.json
+    user = User.query.filter_by(username=data['username']).first()
+    if user and bcrypt.check_password_hash(user.password_hash, data['password']): 
+        access_token = create_access_token(identity=user.id, expires_delta=datetime.timedelta(hours=1))
+        return jsonify({"token": access_token, "message": f"You're logged in as {data['username']}"})
+    return jsonify({"message": "Invaild ID or Password"}), 401
+
+@app.route('/user', methods=['GET'])
+@jwt_required()
+def user(): 
+    user_id = get_jwt_identity()
+    user = User.query.filter_by(id=user_id).first()
+    if user: 
+        return jsonify({"username": user.username})
+    return jsonify({'message': 'No such user'}), 404
+
+@app.route('/memo', methods=['POST'])
+@jwt_required()
+def write_memo(): 
+    user_id = get_jwt_identity()
+    assert request.get_json() is not None
+    content = request.json.get("content")
+    memo = Memo(user_id=user_id, content=content)
+    db.session.add(memo)
+    db.session.commit()
+    return jsonify({"message": "Memo saved"})
+
+@app.route('/memos', methods=['GET'])
+@jwt_required()
+def get_memo(): 
+    user_id = get_jwt_identity()
+    memos = Memo.query.filter_by(user_id=user_id).all()
+    return jsonify([{"id": m.id, "content": m.content} for m in memos])
+
+# Initialize DB
+with app.app_context(): 
+    db.create_all()
+
+if __name__=="__main__": 
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,94 @@
+async function register() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const result = await fetch("/register", {
+        method: "POST", 
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({username, password})
+    });
+    const data = await result.json()
+    alert(data.message);
+}
+
+async function change_login_message() {
+    const token = localStorage.getItem("token");
+    const result = await fetch("/user", {
+        method: "GET", 
+        headers: {"Content-Type": "application/json",
+                  "Authorization": `Bearer ${token}`
+        }
+    })
+    const data = await result.json();
+    if (token) {
+        document.getElementById("login_text").innerHTML = "Hello " + data.username;
+    }
+    else {
+        document.getElementById("login_text").innerHTML = "Login / Register";
+    }
+}
+
+async function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const result = await fetch("/login", {
+        method: "POST", 
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({username, password})
+    });
+    const data = await result.json();
+    alert(data.message);
+    localStorage.setItem("token", data.token);
+    change_login_message();
+    loadMemos();
+}
+
+async function saveMemo() {
+    const token = localStorage.getItem("token");
+    const content = document.getElementById("memo").value;
+    const requestBody = JSON.stringify({content});
+    const result = await fetch("/memo", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json", 
+            "Authorization": `Bearer ${token}`
+        }, 
+        body: requestBody
+    })
+    const data = await result.json();
+    alert(data.message);
+    loadMemos();
+}
+
+async function loadMemos() {
+    const token = localStorage.getItem("token");
+
+    const result = await fetch("/memos", {
+        method: "GET", 
+        headers: {"Authorization": `Bearer ${token}`}
+    });
+
+    const data = await result.json();
+    const memoList = document.getElementById("memos");
+    memoList.innerHTML = "";
+
+    data.forEach(element => {
+        const memoItem = document.createElement("p");
+        memoItem.textContent = element.content;
+        memoList.appendChild(memoItem);
+    });
+}
+
+window.onload = function() {
+    const token = localStorage.getItem("token");
+    change_login_message();
+    if (token) loadMemos();
+}
+
+const inputField = document.getElementById("memo");
+const charCountDisplay = document.getElementById("charCount");
+const maxLength = inputField.maxLength;
+
+inputField.addEventListener("input", () => {
+    const currentLength = inputField.value.length;
+    charCountDisplay.innerHTML = `${currentLength} / ${maxLength}`;
+})
